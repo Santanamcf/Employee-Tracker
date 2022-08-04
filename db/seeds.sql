8332b925b6fb06cc853fb65f267a17fd2928c9ee
Santana Fernandez <santanamcf01@gmail.com>
2022-08-04 16:38:15 -0700

    fixing issues

--- db/seeds.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO department (dpt_name)
-VALUES ()
--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const inquirer = require("inquirer")
 const Choices = require("inquirer/lib/objects/choices")
 const mysql = require("mysql")
 require('console.table')
+// const database = require("../db")
+const database = require('./database');
 //const connection = ()
-
+const db = mysql.createConnection({
+    host: "localhost", 
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_db"
+})
+db.connect(()=>{
+    prompt()
+})
 const promptMessages = {
     viewAllEmployees: "View every employee",
     addEmployee: "Add an employee",
@@ -31,8 +42,56 @@ function prompt(){
         ]
     })
 
-   // .then(ans=>){}
-   //
+    .then(ans=>{
+        console.log(ans.action)
+        if(ans.action=== promptMessages.viewAllEmployees){
+            viewEmployees()
+            prompt()
+        }
+        if(ans.action=== promptMessages.viewAllDepartments){
+            viewDepartments()
+        }
+        if(ans.action=== promptMessages.viewAllRoles){
+            viewRoles()
+        }
+        if(ans.action=== promptMessages.addEmployee){
+            addEmployee()
+        }
+        if(ans.action=== promptMessages.addRole){
+            addRole()
+        }
+        if(ans.action=== promptMessages.addDepartment){
+            addDepartment()
+        }
+        if(ans.action=== promptMessages.updateRole){
+            updateRole()
+        }
+    })
+   
 }
 
-//prompt()
+function viewDepartments(){
+    database.query("SELECT * FROM departments", (err, data)=>{
+        console.table(data)
+        console.log(data)
+        prompt()
+    })
+}
+
+function viewRoles(){
+    database.query("SELECT * FROM role", (err, data)=>{
+        console.table(data)
+        console.log(data)
+        prompt()
+    })
+}
+
+
+
+function viewEmployees(){
+    database.query("SELECT * FROM employees", (err, employees)=>{
+        console.table(employees)
+    //    console.log(data)
+        prompt()
+    })
+}
